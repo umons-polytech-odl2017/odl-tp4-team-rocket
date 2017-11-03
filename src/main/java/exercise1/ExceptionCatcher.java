052de845eb43af4d2426b11914b36e60e81478a5
150780 <julien.dalne@student.umons.ac.be>
2017-11-03 11:07:41 +0100

    feu

--- src/main/java/exercise1/ExceptionCatcher.java
+++ src/main/java/exercise1/ExceptionCatcher.java
@@ -8,6 +8,10 @@ public class ExceptionCatcher {
 	}
 
 	void executeExceptionThrowerSafely(int value) {
-		exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+
+		try {
+			exceptionThrower.throwACustomExceptionWhenValueIs42(value);
+		}catch (Rexep e){}
 	}
+
 }
